80699d790a3328601d13728fc4d0d08ccae6133f
DevoteAI <team@updevoteai.com>
2025-03-25 18:22:45 +0400

    Add source files

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { AuthProvider } from './contexts/AuthContext';
-import { GenerationHistoryProvider } from './contexts/GenerationHistoryContext';
-import { OutlineProvider } from './contexts/OutlineContext';
-import { NewsletterProvider } from './contexts/NewsletterContext';
-import { IdeasProvider } from './contexts/IdeasContext';
-import { ArticlesProvider } from './contexts/ArticlesContext';
-import { BrochureProvider } from './contexts/BrochureContext';
-import { SocialPostsProvider } from './contexts/SocialPostsContext';
-import { ToastProvider } from './contexts/ToastContext';
-import { LeadImportsProvider } from './contexts/LeadImportsContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <AuthProvider>
-      <ToastProvider>
-        <GenerationHistoryProvider>
-          <IdeasProvider>
-            <ArticlesProvider>
-              <OutlineProvider>
-                <SocialPostsProvider>
-                  <BrochureProvider>
-                    <NewsletterProvider>
-                      <LeadImportsProvider>
-                        <App />
-                      </LeadImportsProvider>
-                    </NewsletterProvider>
-                  </BrochureProvider>
-                </SocialPostsProvider>
-              </OutlineProvider>
-            </ArticlesProvider>
-          </IdeasProvider>
-        </GenerationHistoryProvider>
-      </ToastProvider>
-    </AuthProvider>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
